6ce326a3c6f00abb3d9b2c03b03fccf6b96efd48
int <64084503+chul0721@users.noreply.github.com>
2020-09-12 06:15:31 +0000

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
 
 client.on('message', message => {
   if(message.channel.type == 'dm') return
-  if(!message.content.startsWith(process.env.prefix)) return
+  if(!message.content.startsWith(prefix)) return
   if (message.author.bot) return;
   if (!message.guild) return;
 
