d81a66250964a35e359c4540940d9cff289e18a4
int <64084503+chul0721@users.noreply.github.com>
2020-09-12 13:07:46 +0000

    Update clear.js

--- commands/관리 명령어/clear.js
+++ commands/관리 명령어/clear.js
@@ -25,8 +25,7 @@ module.exports = {
         }
 
         message.channel.bulkDelete(deleteAmount + 1, true)
-            const deleted = deleted.size + 1;
-            .then(deleted => message.channel.send(` \`${deleted}\` 개의 메시지를 삭제하였습니다.`))
+            .then(deleted => message.channel.send(` \`${deleted.size + 1}\` 개의 메시지를 삭제하였습니다.`))
             .catch(err => message.reply(`Something went wrong... ${err}`));
     }
 }
