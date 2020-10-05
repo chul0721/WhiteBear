a13d1b079f2b1d9d2de15288e761e1dbe8b4920c
int <64084503+chul0721@users.noreply.github.com>
2020-10-05 09:27:13 +0900

    Add files via upload

--- commands/정보 명령어/Supportserver.js
+++ commands/정보 명령어/Supportserver.js
@@ -3,6 +3,6 @@ module.exports = {
     category: "일반 명령어",
     description: "WhiteBear 서포트 서버 링크",
     run: async (client, message, args) => {
-        const msg = await message.channel.send('https://discord.gg/yGUkV8V');
+        const msg = await message.channel.send('https://discord.gg/WxjQaPK');
     }
 }
