86f2d9af11aa7a4609eb2df7cbf754051ab9322a
int <64084503+chul0721@users.noreply.github.com>
2020-10-13 15:06:59 +0900

    Update index.js

--- index.js
+++ index.js
@@ -139,12 +139,10 @@ client.on('message', message => {
     .addFields(
       { name: '도움', value: 'WhiteBear의 사용법을 알려드려요.' },
       { name: '\u200B', value: '\u200B' },
-      { name: '서포트', value: '서포트 서버의 링크를 보내요.', inline: true },
-      { name: '청소 (갯수)', value: '(갯수)만큼의 메시지를 삭제해요.', inline: true },
-      { name: '초대', value: '초대 링크를 보내요.', inline: true },
-      { name: '정보 (사람 맨션)', value: '맨션한 사람의 정보를 알려드립니다.', inline: true },
-      { name: '서버정보', value: '해당 메시지를 보낸 서버의 정보를 알려드립니다.', inline: true },
-      { name: '레벨', value: '자신의 레벨을 알려줍니다. 만약 레벨 뒤에 사람을 맨션한다면 그 사람의 레벨을 알려줍니다.', inline: true },
+      { name: '화베 {말}', value: '일상대화가 가능합니다!', inline: true },
+      { name: '도움 일반', value: '해당 메시지를 보낸 서버의 정보를 알려드립니다.', inline: true },
+      { name: '도움 관리', value: '해당 메시지를 보낸 서버의 정보를 알려드립니다.', inline: true },
+      { name: '도움 정보', value: '자신의 레벨을 알려줍니다.', inline: true },
       { name: '도움 엔터테인먼트', value: '엔터테인먼트 명령어들을 알려드립니다.', inline: true }
     )
     .setTimestamp()
@@ -152,6 +150,65 @@ client.on('message', message => {
   
 
     message.channel.send(help)
+    return
+  }
+   if (message.content === prefix + ` 도움 관리`) {
+    const manage = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('화베야 도움')
+    .setAuthor('WhiteBear')
+    .setDescription('WhiteBear의 도움말이에요.')
+    .addFields(
+      { name: '도움', value: 'WhiteBear의 사용법을 알려드려요.' },
+      { name: '\u200B', value: '\u200B' },
+      { name: '서포트', value: '서포트 서버의 링크를 보내요.', inline: true },
+      { name: '청소 (갯수)', value: '(갯수)만큼의 메시지를 삭제해요.', inline: true }
+    )
+    .setTimestamp()
+    .setFooter('2020 int ©. All Rights Reserved.');
+  
+
+    message.channel.send(manage)
+    return
+  }
+  if (message.content === prefix + ` 도움 정보`) {
+    const info = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('화베야 도움')
+    .setAuthor('WhiteBear')
+    .setDescription('WhiteBear의 도움말이에요.')
+    .addFields(
+      { name: '도움', value: 'WhiteBear의 사용법을 알려드려요.' },
+      { name: '\u200B', value: '\u200B' },
+      { name: '정보 (사람 맨션)', value: '맨션한 사람의 정보를 알려드립니다.', inline: true },
+      { name: '서버정보', value: '해당 메시지를 보낸 서버의 정보를 알려드립니다.', inline: true },
+      { name: '레벨', value: '자신의 레벨을 알려줍니다.', inline: true }
+    )
+    .setTimestamp()
+    .setFooter('2020 int ©. All Rights Reserved.');
+  
+
+    message.channel.send(info)
+    return
+  }
+  if (message.content === prefix + ` 도움 일반`) {
+    const normal = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('화베야 도움')
+    .setAuthor('WhiteBear')
+    .setDescription('WhiteBear의 도움말이에요.')
+    .addFields(
+      { name: '도움', value: 'WhiteBear의 사용법을 알려드려요.' },
+      { name: '\u200B', value: '\u200B' },
+      { name: '초대', value: '초대 링크를 보내요.', inline: true },
+      { name: '레벨', value: '자신의 레벨을 알려줍니다.', inline: true },
+      { name: '화베 {말}', value: '일상대화가 가능합니다!', inline: true }
+    )
+    .setTimestamp()
+    .setFooter('2020 int ©. All Rights Reserved.');
+  
+
+    message.channel.send(normal)
     return
   }
   if (message.content === prefix + ` 도움 엔터테인먼트`) {
