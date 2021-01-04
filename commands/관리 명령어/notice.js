22ff24e4ba35473be62dd0dcbc0b5110c364f86f
int <64084503+chul0721@users.noreply.github.com>
2021-01-04 13:43:01 +0900

    Update notice.js

--- commands/관리 명령어/notice.js
+++ commands/관리 명령어/notice.js
@@ -4,9 +4,9 @@ module.exports = {
     category: "관리 명령어",
     description: '공지를 보내요.',
     run: async (client, message, args, ops) => {
-        const prefix = "화베야 공지";
+        const prefix = "화베야 공지 ";
         const msg = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
 
-        message.reply(`${msg}`)
+        message.reply(msg)
     }
 }
