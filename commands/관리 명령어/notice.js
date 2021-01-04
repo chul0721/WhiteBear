464798d0f077d5f4185de4465f510c621e3e20d2
int <64084503+chul0721@users.noreply.github.com>
2021-01-04 14:15:35 +0900

    Update notice.js

--- commands/관리 명령어/notice.js
+++ commands/관리 명령어/notice.js
@@ -1,19 +1,25 @@
 const Discord = require('discord.js');
+const manager = require('./manager.json');
 module.exports = {
     name: '공지',
     category: "관리 명령어",
     description: '공지를 보내요.',
     run: async (client, message, args, ops) => {
+        let i;
+        for(i=0;i<=manager.manager.length;i++){
+            if(!message.author.id == manager.manager[i]){
+                return message.channel.send('봇의 관리자만 사용할 수 있어요.')
+            }
+        }
         let channelName = message.guild.channels.cache.find(channel => channel.name.toLowerCase().includes("화베"))
         if(!channelName) return
-        let title = args[2]
-        let msg = args[3]
+        let title = args[2];
+        let msg = args[3];
 
         const noticeEmbed = new Discord.MessageEmbed()
         .setColor('RANDOM')
         .setTitle('WhiteBear 공지')
         .setAuthor('WhiteBear')
-        .setDescription('공지 사항')
         .addFields(
           { name: `${title}`, value: `${msg}`, inline: true },
         )
