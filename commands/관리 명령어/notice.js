a2037d4690a7a54ad1dbac25030448c291699bae
int <64084503+chul0721@users.noreply.github.com>
2021-01-04 13:52:21 +0900

    Update notice.js

--- commands/관리 명령어/notice.js
+++ commands/관리 명령어/notice.js
@@ -4,12 +4,10 @@ module.exports = {
     category: "관리 명령어",
     description: '공지를 보내요.',
     run: async (client, message, args, ops) => {
-        const prefix = "화베야 공지 ";
-        const msg = args[0].slice(prefix.length).toLowerCase(); // case INsensitive, without prefix
         let channelName = message.guild.channels.cache.find(channel => channel.name.toLowerCase().includes("화베"))
 
         if(channelName){
-            channelName.send(msg)
+            channelName.send(message)
         }
     }
 }
