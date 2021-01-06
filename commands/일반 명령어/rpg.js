0fe4155b9632b9c404b5455a6ce586ebff329275
int <64084503+chul0721@users.noreply.github.com>
2021-01-06 14:29:44 +0900

    Update rpg.js

--- commands/일반 명령어/rpg.js
+++ commands/일반 명령어/rpg.js
@@ -11,15 +11,16 @@ module.exports = {
         if(args[0] == "가입") {
             if(userInfo.user){
                 return message.channel.send("이미 도박에 가입한 유저입니다")
+            } else {
+                fs.writeFile("./money.json", JSON.stringify(user), (err) => {
+                    if (err) {
+                            return console.error(err)
+                    }
+                });
+                return message.channel.send("가입을 완료하였습니다.").then(
+                    console.log("가입 완료 : " + user)
+                )
             }
-            fs.writeFile("./money.json", JSON.stringify(user), (err) => {
-                if (err) {
-                    return console.error(err)
-                }
-            });
-            return message.channel.send("가입을 완료하였습니다.").then(
-                console.log("가입 완료 : " + user)
-            )
         }
         if(!userInfo.user.id) {
             return message.channel.send("가입되지 않은 유저입니다.").then(
